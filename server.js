1b6e68295ba9b0e426df9ca084e954f54ea37a59
bala-arunachalam <bala.arunachalam@gmail.com>
2017-02-23 00:39:05 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,9 +128,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name:name', function (req, res) {
+app.get('/submit-name/:name', function(req, res) {
     //Get the name from the request
-    var name = req.param.name;
+    var name = req.params.name;
     names.push(name);
     //JSON JS Obj Notation
     res.send(JSON.stringify(names));
