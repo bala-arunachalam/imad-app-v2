f8863d1d518d67359fb76ef094a0f9d53dfbc8ee
bala-arunachalam <bala.arunachalam@gmail.com>
2017-02-27 02:20:47 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,13 @@
-//console.log('Loaded!');
-//var element = document.getElementById('main-text');
-//element.innerHTML = 'New Value';
-//
-//// Move the image
-//var img = document.getElementById('madi');
-//var marginLeft = 0;
-//function moveRight() {
-//    marginLeft = marginLeft + 5;
-//    img.style.marginLeft = marginLeft + 'px';
-//}
-//img.onclick = function () {
-//    var interval = setInterval(moveRight, 50);
-//    img.style.marginLeft = '50px';
-//};
+//Counter code
 var button = document.getElementById('counter');
 
 button.onclick = function () {
+    
     // create a request object
     var request = new XMLHttpRequest();
+    
     //capture the response and store in a variable
-    request.onreadyStatechange = function () {
+    request.onreadystatechange = function () {
         if (request.readystate === XMLHttpRequest.DONE) {
             //take some action
             if (request.status === 200) {
@@ -30,6 +18,7 @@ button.onclick = function () {
         }
         //Not done yet
     };
+    
     //make the request
     request.open('GET', 'http://bala-arunachalam.imad.hasura-app.io/counter', true);
     request.send(null);
