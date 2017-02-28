cab2e05af7d117d089f037688dc9d675e596f2ca
bala-arunachalam <bala.arunachalam@gmail.com>
2017-02-28 08:24:22 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,23 +4,25 @@ var button = document.getElementById('counter');
 button.onclick = function () {
     
     // create a request object
-    var request = new XMLHttpRequest();
-    
+    var request = new XMLHttpRequest(); 
+    {
     //capture the response and store in a variable
-    request.onreadystatechange = function () {
-        if (request.readystate === XMLHttpRequest.DONE) {
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLhttpRequest.DONE) 
             //take some action
+            {
             if (request.status === 200) {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHTML = counter;
+                span.innerHTML = counter.toString();
             }
         }
-        //Not done yet
     };
+        //Not done yet
+    }
     
     //make the request
-    request.open('GET', 'http://bala-arunachalam.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://bala-arunachalam.imad.hasura-app.io/counter');
     request.send(null);
 };
 
