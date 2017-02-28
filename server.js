dca0cdba6ee1cb90ceaa4dce762daa07ea01917a
bala-arunachalam <bala.arunachalam@gmail.com>
2017-02-28 05:36:47 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req,res) {
     counter = counter+1;
-    res.send(counter.toSring());
+    res.send(counter);
 });
 
 
