1acb1265c8acfbc1b1b24f1c9028412b1ee17735
bala-arunachalam <bala.arunachalam@gmail.com>
2017-02-28 05:34:46 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,8 +52,6 @@ content: `
 }
 };
 
-
-
 function createTemplate (data) {
 var title = data.title;
 var date = data.date;
@@ -62,36 +60,29 @@ var content = data.content;
 
 var htmlTemplate = `
 <html>
-<head>
-<title>
- ${title}
- 
-</title>
-
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<link href="/ui/style.css" rel="stylesheet" />
-</head>
+    <head>
+        <title>  ${title} </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
 <body>
 
-<div class="container">
-    <div>
-     <a href="/">Home</a>
+    <div class="container">
+        <div>
+        <a href="/">Home</a>
+        </div>
+    <hr/>
+        <h3>
+         ${heading}
+         </h3>
+         <div>
+          ${date}
+         </div>
+         <div>
+          ${content}
+         </div>
     </div>
- <hr/>
- <h3>
-     ${heading}
- </h3>
-     <div>
-      ${date}
-     </div>
-     <div>
-      ${content}
-     </div>
-
-
-</div>
 </body>
-
 </html>
 `;
 return htmlTemplate;
@@ -137,9 +128,6 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
