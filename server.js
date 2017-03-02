0026b7e7dd83555f5de5990bf01bb20099d64863
bala-arunachalam <bala.arunachalam@gmail.com>
2017-03-02 01:31:46 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/submit-name/:name', function(req, res) { //URL: /submit-name?name-xxxx
     res.send(JSON.Stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
   var articleName = req.params.articleName;
