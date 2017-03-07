dcf00cb2a5690b1fec98746a1093e409472f19bf
bala-arunachalam <bala.arunachalam@gmail.com>
2017-03-07 10:34:12 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,14 +143,14 @@ app.get('/submit-name', function(req, res) { //URL: /submit-name?name-xxxxx
 app.get('articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
-    pool.query("SELECT * from article where title = '" + req.params.articleName +"'", function(err,result){
+    pool.query("SELECT * from article where title = '"+ req.params.articleName +"'", function(err, result) {
         if (err) {
           res.status(500).send(err.toString());
       } else {
           if (result.rows.length === 0) {
           res.status(404).send('Article not found');
       } else {
-          var articleData = result.rows[1];
+          var articleData = result.rows[0];
             res.send(createTemplate(articleData));
       }
       }
