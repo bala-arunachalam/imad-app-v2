8e9858e6fbde884e54183d670816577eb02a4fa4
bala-arunachalam <bala.arunachalam@gmail.com>
2017-03-08 01:43:10 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,20 +113,17 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/test-db', function(req,res) {
   //make a select request
     pool.query('SELECT * FROM test',function(err,result){
        if (err){
            res.status(500).send(err.toString());
        } else{
            res.send(JSON.stringify(result.rows));
-           
-       }
+    }
     });
   //return a response
-  
-}
-);
+});
 
 var counter = 0;
 app.get('/counter', function(req,res) {
