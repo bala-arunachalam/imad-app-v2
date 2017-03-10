69f9c191055b926e277b2b424696f792ab8e617d
bala-arunachalam <bala.arunachalam@gmail.com>
2017-03-10 00:53:24 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,53 +17,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-
-var articles = {
-'article-one': {
-title: 'Article One | Bala Arunachalam',
-heading: 'Article One',
-date: 'Feb 20, 2016',
-content: ` 
-     <p>
-        This is the content for my first article. This is the content for my first article. This is the content for my first
-        article. This is the content for my first article. This is the content for my first article. This is the content for my 
-        first article.
-     </p>
-     
-     <p>
-        This is the content for my first article. This is the content for my first article. This is the content for my first
-        article. This is the content for my first article. This is the content for my first article. This is the content for my 
-        first article.
-     </p>
-     
-   
-     <p>
-        This is the content for my first article. This is the content for my first article. This is the content for my first
-        article. This is the content for my first article. This is the content for my first article. This is the content for my 
-        first article.
-     </p>`
-    
-},
-'article-two': { 
-title: 'Article Two | Bala Arunachalam',
-heading: 'Article TWO',
-date: 'Feb 22, 2016',
-content: ` 
-         <p>
-            This is the content for my second article. 
-         </p>`
-    },
-'article-three': { 
-title: 'Article Three | Bala Arunachalam',
-heading: 'Article THREE',
-date: 'Feb 25, 2016',
-content: ` 
- <p>
-    This is the content for my third article. 
- </p>`
-}
-};
-
 function createTemplate (data) {
 var title=data.title;
 var date=data.date;
