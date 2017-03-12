18bba796d21ae516e368d102674f830257fd032c
bala-arunachalam <bala.arunachalam@gmail.com>
2017-03-12 06:46:43 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,7 +62,36 @@ submit.onclick = function(){
 };
 
 
+//Submit Username and Password
 
+var submit = document.getElementById('submit_btn2');
+submit.onclick=function(){
+//Make a request to the server and send the name
+   var request = new XMLHttpRequest();
+//Capture list of names and render as list
+  request.onreadystatechange = function()
+  {
+    if (request.readyState === XMLHttpRequest.DONE){
+          //take action
+          if(request.status === 200){
+              console.log('User loggedin');
+              alert('Logged in successfully');
+            }else if(request.status === 403){
+                alert('Username/Password incorrect');
+            }else if(request.status === 500){
+                alert('Something went wrong on the server');
+            }
+          }
+      };
+ //Make Request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://bala-arunachalam.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username , password: password}));
+};
 
 
 
